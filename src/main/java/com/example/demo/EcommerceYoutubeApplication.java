e5b47768755914d2f2bd37bebc1b5321eb0a157d
VishwajeetPawargit config --global user.email vishwjitpawar340@gmail.com <vishwjitpawar340@gmail.com>
2025-05-23 16:30:47 +0530

    p

--- src/main/java/com/example/demo/EcommerceYoutubeApplication.java
+++ src/main/java/com/example/demo/EcommerceYoutubeApplication.java
@@ -1,14 +1,31 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 
 @SpringBootApplication
-public class EcommerceYoutubeApplication {
+public class EcommerceYoutubeApplication implements ApplicationListener<ApplicationReadyEvent> {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommerceYoutubeApplication.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(EcommerceYoutubeApplication.class);
 
+    @Value("${spring.datasource.url:}")
+    private String datasourceUrl;
+
+    @Value("${spring.datasource.username:}")
+    private String datasourceUsername;
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceYoutubeApplication.class, args);
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        logger.info("Application started with datasource URL: {}", datasourceUrl);
+        logger.info("Application started with datasource username: {}", datasourceUsername);
+    }
 }
