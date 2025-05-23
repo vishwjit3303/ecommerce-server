93c9668bce86e35d262820be0d3dfb04945d56e5
VishwajeetPawargit config --global user.email vishwjitpawar340@gmail.com <vishwjitpawar340@gmail.com>
2025-05-23 16:18:43 +0530

    up

--- src/main/java/com/example/demo/config/ApplicationStartupListener.java
+++ src/main/java/com/example/demo/config/ApplicationStartupListener.java
@@ -2,18 +2,25 @@ package com.example.demo.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.context.event.ApplicationFailedEvent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ApplicationStartupListener implements ApplicationListener<ApplicationFailedEvent> {
+public class ApplicationStartupListener implements ApplicationListener<ApplicationReadyEvent> {
 
     private static final Logger logger = LoggerFactory.getLogger(ApplicationStartupListener.class);
 
+    @Value("${spring.datasource.url:}")
+    private String datasourceUrl;
+
+    @Value("${spring.datasource.username:}")
+    private String datasourceUsername;
+
     @Override
-    public void onApplicationEvent(ApplicationFailedEvent event) {
-        Throwable exception = event.getException();
-        logger.error("Application failed to start due to exception: ", exception);
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        logger.info("Application started with datasource URL: {}", datasourceUrl);
+        logger.info("Application started with datasource username: {}", datasourceUsername);
     }
 }
